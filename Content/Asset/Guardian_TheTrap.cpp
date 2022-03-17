bd9cd46ca0df2ade4345e95e6cdd51aa7f7422a4
Yueting Weng <WengYuehTing@163.com>
2022-03-17 21:13:55 +0800

    Add files via upload

---
+++ Content/Asset/Guardian_TheTrap.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Guardian_TheTrap.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Guardian_TheTrap, "Guardian_TheTrap" );
