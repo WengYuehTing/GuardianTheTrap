8e7841eed4a2e4df44b9181d85c9ac895f780f44
Yueting Weng <WengYuehTing@163.com>
2022-05-06 22:06:55 +0800

    deleted files

--- Content/Asset/Guardian_TheTrap.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Guardian_TheTrap.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Guardian_TheTrap, "Guardian_TheTrap" );
